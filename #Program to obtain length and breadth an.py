4fbf2ea9c22fe21eb26291dc70d74b27e19019e0
Divyesh Kamble <kambledivyesh18@gmail.com>
2026-07-25 00:40:03 +0530

    Python code file

---
+++ #Program to obtain length and breadth an.py
@@ -0,0 +1,7 @@
+#Program to obtain length and breadth and calculate the the area and perimetre
+l = float(input("Enter the length "))
+b = float(input("Enter the breadth "))
+a = ("The area of given length",l,"and breadth",b,"is" ,l*b)
+p = ("The perimetre of given length",l,"and breadth",b,"is",2*l+b )
+print (a)
+print (p)
