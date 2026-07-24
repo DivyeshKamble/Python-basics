6257390a64e5d2a502161a18025fbd392bd266ba
Divyesh Kamble <kambledivyesh18@gmail.com>
2026-07-25 00:32:00 +0530

    Added the python code

---
+++ #Code to find the Square root of given number.py
@@ -0,0 +1,4 @@
+#Code to find the Square root of given number
+num = float(input("Enter the number of which you want to find the square root "))
+sqr = num**(1/2)
+print ("the Square root of",num, "is",sqr,) 
