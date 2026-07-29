53d04ca7014fb1292ccbd12de6434aea18060bfe
Divyesh Kamble <kambledivyesh18@gmail.com>
2026-07-29 11:45:48 +0530

    Added the python file

---
+++ Odd even split.py
@@ -0,0 +1,6 @@
+# This program is used to check if the number is odd or even.
+num = int(input("Enter any number which you want to check if it is odd or even: "))
+if num % 2==0 :
+    print(num , "is an even number")
+else:
+    print(num , "is an odd number")
