f8a870d88f088e26bd96c032143fc6a879bc3f4c
Divyesh Kamble <kambledivyesh18@gmail.com>
2026-08-05 22:26:45 +0530

    Added the python code

---
+++ enter names of 3 fav movies and store them in list.py
@@ -0,0 +1,11 @@
+# This program will take the names of your 3 favorite movies and store them in a list.
+movies = []
+movies.append(input("Enter your first favorite movie: "))
+movies.append(input("Enter your second favorite movie: ")) 
+movies.append(input("Enter your third favorite movie: "))
+print("Your favorite movies are: ", movies) 
+
+
+
+
+
